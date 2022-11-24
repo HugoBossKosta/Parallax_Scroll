f11b8a14d2f28f4ba854854f586eab10660ddc4c
Konstantin Kuznetsov <hugobossscorpion@gmail.com>
2022-11-24 03:15:37 +0300

    add sound

--- js/app.js
+++ js/app.js
@@ -6,4 +6,26 @@ gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 ScrollSmoother.create({
     wrapper: '.wrapper',
     content: '.container'
-})
+});
+
+
+let soundButton = document.querySelector('.soundbutton'),
+    audio = document.querySelector('.audio')
+
+    soundButton.addEventListener('click', e => {
+        soundButton.classList.toggle('paused');
+        audio.paused ? audio.play() : audio.pause();
+        audio.volume = 0.5;
+    });
+    // Убираем фокус
+    window.onfocus = function() {
+        // soundButton.classList.contains('paused') ? audio.pause() : audio.play();
+        audio.pause();
+        if (audio.pause) {
+            soundButton.classList.add('paused');
+        }        
+    };
+    // покидаем страницу
+    window.onblur = function() {
+        audio.pause();
+    };
